a249e0bb23f2898afac7e4b5423c048cc73233d3
simlesam <162280899+simlesam@users.noreply.github.com>
2024-03-05 16:49:05 +0800

    sever.py

--- sever.py
+++ sever.py
@@ -41,6 +41,6 @@ with st.form("my_form"):
       params = {
           'name':'工作表1',
           'top':'true',
-          'data':[room,date,time,name,number]
+          'data':'[room,123,time,name,number]'
           }
       web = requests.get(url=url, params=params)
